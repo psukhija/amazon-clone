d71fcbce43eccd145f2e6b55a7b5c0c897a04081
psukhija <91307454+psukhija@users.noreply.github.com>
2021-12-08 15:44:15 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter total number of elements ";
+    cin>>n;
+    int a[n];
+    cout<<"enter elements ";
+    for( i=0;i<n;i++)
+    cin>>a[i];
+    int counter =1;
+    while(counter<n){
+        for(i=0;i<n-counter;++i){
+            if(a[i]>a[i+1]){
+                int temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+        }
+        counter++;
+    }
+     for( i=0;i<n;i++)
+     cout<<a[i]<<" ";
+    return 0;
+}
